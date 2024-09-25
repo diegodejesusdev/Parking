953e06a17338dfcfa3b93128f44788d841bd4eb8
Diego Dejesus <diegodejesuspersonal@gmail.com>
2024-09-25 14:48:53 -0500

    Se modifico index.php, los controladores ya funcionan

--- index.php
+++ index.php
@@ -2,4 +2,18 @@
     require 'vendor/autoload.php';
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
-    header('Location: inicio.php');
+
+    require_once 'controllers/UsuarioController.php';
+    $controller = isset($_GET['controller']) ? $_GET['controller'] : null;
+    $action = isset($_GET['action']) ? $_GET['action'] : null;
+
+    if ($controller == 'usuario'){
+        $usuarioController = new UsuarioController();
+        if($action == 'obtenerTodos') {
+            $usuarioController->obtenerTodos();
+        }else{
+            echo 'Acción no encontrada';
+        }
+    }else{
+        echo 'Controlador no encontrado';
+    }
